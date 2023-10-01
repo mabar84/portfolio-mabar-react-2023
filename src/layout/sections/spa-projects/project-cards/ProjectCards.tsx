6504d3a5011ffb25c4533ce26cecf454f47c426f
mabar84 <mabar@tut.by>
2023-10-01 15:51:54 +0300

    before electron

--- src/layout/sections/spa-projects/project-cards/ProjectCards.tsx
+++ src/layout/sections/spa-projects/project-cards/ProjectCards.tsx
@@ -9,7 +9,7 @@ export const ProjectCards = () => {
             {myTheme.state.projectsCards.map((el, ind) => {
                 return <StyledProjectCard key={ind} title={el.name}>
                     <div>
-                        <img src={require(`./../../../../assets/projects-images/${el.name}.webp2`)}
+                        <img src={require(`./../../../../assets/projects-images/${el.name}.webp`)}
                              alt={el.name}/>
                         <StyledProjectCardTitle>
                             {el.title}
